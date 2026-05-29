3d8cd84ce44282502fc1a65fe1a5cdd6aa866116
bowen <botgroff@gmail.com>
2026-05-29 14:56:42 -0400

    gcd strings checks both strings with candidate that they divide evenly

--- 75/Easy/GCDStrings.cpp
+++ 75/Easy/GCDStrings.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -8,21 +9,37 @@ public:
     string gcdOfStrings(string str1, string str2) {
         int str1_length = str1.size();
         int str2_length = str2.size();
-        int length;
 
+        string larger_string;
+        string smaller_string;
         string substring;
 
         if(str1_length > str2_length) {
-            length = str1_length;
+            larger_string = str1;
+            smaller_string = str2;
         } else {
-            length = str2_length;
+            larger_string = str2;
+            smaller_string = str1;
         }
 
-        for(int i = 0; i < length; i++) {
-            if(str1.substr(0,i) == str2.substr(0, i)) {
-                substring = str1.substr(0,i);
+        for(int i = 1; i < smaller_string.length() + 1; i++) {
+             bool isvalid = true;
+
+            if(larger_string.length() % larger_string.substr(0, i).length() == 0 && smaller_string.length() % smaller_string.substr(0,i).length() == 0 && str1.substr(0,i) == str2.substr(0,i)) {
+                string candidate = larger_string.substr(0,i);
+                //checking the candidate divides evenly into both str1 and str2
+                for(int j = 0; j < larger_string.length(); j += candidate.length()) {
+                    if(larger_string.substr(j, candidate.length()) != candidate)
+                        isvalid = false;
+                    if(j < smaller_string.length() && smaller_string.substr(j, candidate.length()) != candidate)
+                        isvalid = false;
+                }
+
+                if(isvalid)
+                    substring = candidate;
             }
         }
+        return substring;
     }
 };
 
