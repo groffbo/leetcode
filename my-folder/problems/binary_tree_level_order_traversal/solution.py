ee93008ad226c55b071d2e1f33fb3b7cb988cc22
bowen <botgroff@gmail.com>
2026-09-01 22:52:04 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (python)

--- my-folder/problems/binary_tree_level_order_traversal/solution.py
+++ my-folder/problems/binary_tree_level_order_traversal/solution.py
@@ -10,29 +10,32 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
+        #level order traversal means bfs printing
+        #bfs means to explore the entire level, and then the children
+        #so we do it with a queue?
+        #but we need a way to group each level
+        
+
         q = []
         ret = []
 
         if root:
             q.append(root)
-            ret.append([root.val])
 
         while q:
-            curr = []
-            for n in range(len(q)):
-                tmp = q.pop(0)
-                if tmp.left:
-                    q.append(tmp.left)
-                    curr.append(tmp.left.val)
+            length = len(q)
+            level = []
+            #process that many nodes to do the entire level
+            for n in range(length):
+                val = q.pop(0)
+                level.append(val.val)
+                if val.left:
+                    q.append(val.left)
+                if val.right:
+                    q.append(val.right)
 
-                if tmp.right:
-                    q.append(tmp.right)
-                    curr.append(tmp.right.val)
-            if curr:
-                ret.append(curr)
+            ret.append(level)
 
         return ret
-        
-        
 
         
