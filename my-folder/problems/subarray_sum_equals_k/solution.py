e24c5e8fd00ef594de8dce7ee672786196abbb6d
bowen <botgroff@gmail.com>
2026-09-02 16:12:32 +0000

    Sync LeetCode submission - Subarray Sum Equals K (python3)

--- my-folder/problems/subarray_sum_equals_k/solution.py
+++ my-folder/problems/subarray_sum_equals_k/solution.py
@@ -4,39 +4,30 @@ class Solution:
         # count = 0
 
         # for i, n in enumerate(nums):
-        #     #at each index
         #     right = i
         #     total = 0
-            
         #     while right < len(nums):
         #         total += nums[right]
         #         right += 1
         #         if total == k:
         #             count += 1
 
-        
         # return count
 
-        #optimized solution
-
-        #save off last calculated sum 
-
-        #store ramping sum in list
-        
-        #key: prefix sum value: count of that prefix sum
         hashmap = {}
+        count = 0
+        running_sum = 0
+
         hashmap[0] = 1
 
-        total = 0
-        count = 0
+        for n in nums:
+            running_sum += n
+            if (running_sum - k) in hashmap:
+                count += hashmap[running_sum - k]
 
-        for i, n in enumerate(nums):
-            total += n
-            if (total - k) in hashmap:
-                count += hashmap[total-k]
-            if total in hashmap:
-                hashmap[total] += 1
+            if running_sum in hashmap:
+                hashmap[running_sum] += 1
             else:
-                hashmap[total] = 1
+                hashmap[running_sum] = 1
 
         return count
